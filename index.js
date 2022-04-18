decc0611e8343a07138c9cc8a81ef7ef15f4a561
GiacomoBorsellino <giacomoborsellino@hotmail.it>
2022-04-18 17:10:32 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const one = () => {
+    let audio = new Audio('./sound/one.mp3');
+    audio.play();
+}
+
+const two = () => {
+    let audio = new Audio('./sound/two.mp3');
+    audio.play();
+}
+
+const three = () => {
+    let audio = new Audio('./sound/three.mp3');
+    audio.play();
+}
+
+const four = () => {
+    let audio = new Audio('./sound/four.mp3');
+    audio.play();
+}
+
+const five = () => {
+    let audio = new Audio('./sound/five.mp3');
+    audio.play();
+}
+
+const six = () => {
+    let audio = new Audio('./sound/six.mp3');
+    audio.play();
+}
+
